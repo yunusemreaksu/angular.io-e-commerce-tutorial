047dad224237c12db94fe75b811f971b3a8f563f
Yunus Emre Aksu <emre_aksu@live.com>
2023-01-06 16:00:35 +0300

    Created new alert feature that uses product data from the product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, TopBarComponent],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductAlertsComponent,
+  ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
